86032577ec9e9740646d82a23995083e69da61d0
mayefa10 <mayefa10@yahoo.com>
2023-05-24 15:08:05 -0500

    manage state by Reducer Hook

--- src/screen/HomeScreen.jsx
+++ src/screen/HomeScreen.jsx
@@ -1,35 +1,68 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useReducer, useState } from "react";
 //import data from '../data'
-import { Link } from 'react-router-dom'
-import axios from 'axios';
+import { Link } from "react-router-dom";
+import axios from "axios";
+import logger from "use-reducer-logger";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "FETCH_REQUEST":
+      return { ...state, loading: true };
+    case "FETCH_SUCCESS":
+      return { ...state, products: action.payload, loading: false };
+    case "FETCH_FAIL":
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
 
 export default function HomeScreen() {
-  const [products,setProducts]=useState([]);
-  useEffect(()=>{
+  const [{ loading, error, products }, dispatch] = useReducer(logger(reducer), {
+    products: [],
+    loading: true,
+    error: "",
+  });
+  //const [products, setProducts] = useState([]);
+  useEffect(() => {
     const fetchData = async () => {
-      const result =await axios.get('/api/products');
-      setProducts(result.data);
+      dispatch({ type: "FETCH_REQUEST" });
+      try {
+        const result = await axios.get("/api/products");
+        dispatch({ type: "FETCH_SUCCESS", payload: result.data });
+      } catch (err) {
+        dispatch({ type: "FETCH_FAIL", payload: err.message });
+      }
     };
     fetchData();
-  },[]);
+  }, []);
   return (
     <div>
-        <h1> Featured Products </h1>
-    <div className="products">
-      {products.map((product) => (
-        <div className="product" key={product.slug}>
-          <Link href={`/product/${product.slug}`}>
-            <img src={product.image} alt={product.name} />
-          </Link>
-          <div className="product-info">
-          <Link href={`/product/${product.slug}`}>
-            <p>{product.name}</p>
-          </Link>
-            <p><strong>${product.price}</strong></p>
-            <button>Add to cart </button>
-          </div>
-        </div>
-      ))}
-    </div></div>
-  )
+      <h1> Featured Products </h1>
+      <div className="products">
+        {loading ? (
+          <div>Loading...</div>
+        ) : error ? (
+          <div>{error}</div>
+        ) : (
+          products.map((product) => (
+            <div className="product" key={product.slug}>
+              <Link href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </Link>
+              <div className="product-info">
+                <Link href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </Link>
+                <p>
+                  <strong>${product.price}</strong>
+                </p>
+                <button>Add to cart </button>
+              </div>
+            </div>
+          ))
+        )}
+      </div>
+    </div>
+  );
 }
