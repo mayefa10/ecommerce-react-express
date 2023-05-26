69bcfaa1cefb2d9ecfb02b6f7fe41904a1ce30bc
mayefa10 <mayefa10@yahoo.com>
2023-05-26 18:26:12 -0500

    add bootstrap in the screen HomeScreen, create component Rating, Product

---
+++ src/components/Product.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import { Link } from "react-router-dom";
+import Rating from "./Rating";
+
+export default function Product(props) {
+  const { product } = props;
+  return (
+    <Card >
+      <Link href={`/product/${product.slug}`}>
+        <img src={product.image} className="card-img-top" alt={product.name} />
+      </Link>
+      <Card.Body>
+        <Link href={`/product/${product.slug}`}>
+          <Card.Title>{product.name}</Card.Title>
+        </Link>
+        <Rating rating ={product.rating} numReviews={product.numReviews}/>
+        <Card.Text>${product.price}</Card.Text>
+        <Button>Add to cart </Button>
+      </Card.Body>
+    </Card>
+  );
+}
